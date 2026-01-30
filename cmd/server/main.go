36e0fab1498c888c02c9bfab5cb0b6df7f166734
Temych228 <242098@astanait.edu.kz>
2026-01-30 16:58:36 +0500

    create postgress connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,17 @@
-package server
+package main
+
+import (
+	"log"
+
+	"github.com/AlikhanF2006/Final_project/configs"
+	"github.com/AlikhanF2006/Final_project/pkg/db"
+)
+
+func main() {
+	configs.LoadConfig()
+	db.Connect()
+	defer db.Close()
+
+	log.Println("server started")
+	select {}
+}
